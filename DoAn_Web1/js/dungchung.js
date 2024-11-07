55e05ab2bab53853d9566d2b8345b4a02c35d34a
ChiThien193 <chithientb2022@gmail.com>
2024-11-07 22:46:11 +0700

    yyyyyyyyyyyyyyyy

--- DoAn_Web1/js/dungchung.js
+++ DoAn_Web1/js/dungchung.js
@@ -52,6 +52,7 @@ function themVaoGioHang(masp, tensp) {
         });
     }
 
+    console.log('Updated user products:', user.products);
     animateCartNumber();
     addAlertBox('Đã thêm ' + tensp + ' vào giỏ.', '#17c671', '#fff', 3500);
 
